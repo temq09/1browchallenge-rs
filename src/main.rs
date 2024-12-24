a208bbcd7e88c646b10eb3253c7114d18cfbac85
artemushakov <hello@temq.dev>
2024-12-24 18:00:12 +0000

    initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
+    cmp::{max, min},
     fs::File,
     io::{BufReader, Error, Read},
     thread,
     time::Instant,
 };
+
+use data_structures::SplitHashMap;
 
 fn main() {
     let start = Instant::now();
@@ -19,18 +22,31 @@ fn naive_implementastion() -> Result<(), Error> {
     let mut counter = 0;
 
     thread::scope(|s| {
-        for _ in 0..6 {
-            s.spawn(|| {
-                println!("test");
-            });
-        }
+        //for _ in 0..6 {
+        //    s.spawn(|| {
+        //        println!("test");
+        //    });
+        //}
 
-        let mut buf = [0_u8; 1024 * 100];
+        let mut leftovers = Vec::new();
+        let mut data_holder = DataHolder::new();
         loop {
+            let mut buf = [0_u8; 1024 * 1000];
             let count = reader.read(&mut buf).unwrap();
             if count == 0 {
                 break;
             }
+            let non_complete_data_index = extract_completed_data(&buf);
+            let mut new_leftovers: Vec<u8> =
+                Vec::with_capacity(max(0, count - non_complete_data_index));
+            new_leftovers.extend(&buf[non_complete_data_index..]);
+
+            // send buffer to the queue to parse
+            let raw_data = RawData::new(leftovers.clone(), buf, non_complete_data_index);
+            leftovers.clear();
+            leftovers.extend(new_leftovers);
+            data_holder.append(&raw_data);
+
             counter += 1;
         }
     });
@@ -39,16 +55,168 @@ fn naive_implementastion() -> Result<(), Error> {
     Ok(())
 }
 
-fn extract_completed_data(data: &[u8]) {}
+fn extract_completed_data(data: &[u8]) -> usize {
+    let mut index = data.len() - 1;
+    while index != 0 {
+        if data[index] == b'\n' {
+            break;
+        }
+        index -= 1;
+    }
+    index
+}
 
 struct StationReading {
     pub name: String,
     pub temperature: i16,
 }
 
+#[derive(Clone)]
 struct TotalReading {
     pub name: String,
     pub min_temp: i16,
     pub max_temp: i16,
-    pub mean_temp: i16,
+    pub sum_temp: i64,
+    pub temp_reading_count: u32,
+}
+
+impl TotalReading {
+    fn new(name: String, tmp_value: i16) -> Self {
+        TotalReading {
+            name,
+            min_temp: tmp_value,
+            max_temp: tmp_value,
+            sum_temp: tmp_value as i64,
+            temp_reading_count: 1,
+        }
+    }
+
+    fn add(&mut self, other: &TotalReading) {
+        self.max_temp = max(self.max_temp, other.max_temp);
+        self.min_temp = min(self.min_temp, other.max_temp);
+        self.sum_temp += other.sum_temp;
+        self.temp_reading_count += other.temp_reading_count;
+    }
+}
+
+struct RawData {
+    pub data_prefix: Vec<u8>,
+    data: [u8; 1024000],
+    end_index: usize,
+}
+
+impl RawData {
+    fn new(data_prefix: Vec<u8>, data: [u8; 1024000], end_index: usize) -> Self {
+        RawData {
+            data,
+            end_index,
+            data_prefix,
+        }
+    }
+    fn get_full_data_slice(&self) -> &[u8] {
+        &self.data[..self.end_index]
+    }
+}
+
+struct DataHolder {
+    data: SplitHashMap,
+}
+
+pub(crate) mod data_structures {
+    use std::collections::HashMap;
+
+    use crate::TotalReading;
+
+    pub(crate) struct SplitHashMap {
+        data: HashMap<char, HashMap<String, TotalReading>>,
+    }
+
+    impl SplitHashMap {
+        pub(crate) fn new() -> Self {
+            SplitHashMap {
+                data: HashMap::new(),
+            }
+        }
+
+        pub(crate) fn get_mut(&mut self, name: &str) -> Option<&mut TotalReading> {
+            name.chars()
+                .next()
+                .and_then(|symbol| self.data.get_mut(&symbol))
+                .map(|table| table.get_mut(name))?
+        }
+
+        pub(crate) fn insert(&mut self, name: String, value: TotalReading) {
+            let first_symbol = name.chars().next().expect("Name must not be empty");
+            match self.data.get_mut(&first_symbol) {
+                Some(table) => {
+                    let _ = table.insert(name, value);
+                }
+                None => {
+                    let mut table = HashMap::new();
+                    table.insert(name, value);
+                    let _ = self.data.insert(first_symbol, table);
+                }
+            };
+        }
+
+        pub(crate) fn merge(&mut self, other: SplitHashMap) {
+            for (key, value) in other.data.iter() {
+                let current_readings = match self.data.get_mut(key) {
+                    Some(reading) => reading,
+                    None => {
+                        let table = HashMap::new();
+                        self.data.insert(*key, table);
+                        self.data.get_mut(key).unwrap()
+                    }
+                };
+                for reading in value.values() {
+                    match current_readings.get_mut(&reading.name) {
+                        Some(current_reading) => current_reading.add(reading),
+                        None => {
+                            let _ = current_readings.insert(reading.name.clone(), reading.clone());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl DataHolder {
+    fn new() -> Self {
+        DataHolder {
+            data: SplitHashMap::new(),
+        }
+    }
+
+    fn append(&mut self, raw_data: &RawData) {
+        let mut buffer = String::new();
+        //let mut buffer = String::from_utf16(raw_data.data_prefix.as_slice()).unwrap();
+        let mut table = SplitHashMap::new();
+
+        for element in raw_data.get_full_data_slice() {
+            match element {
+                0x0A => {
+                    if let Some((name, value)) = buffer.split_once(';') {
+                        let value = (value.parse::<f32>().unwrap() * 10.0) as i16;
+                        match table.get_mut(name) {
+                            Some(raw_value) => {
+                                raw_value.min_temp = min(value, raw_value.min_temp);
+                                raw_value.max_temp = max(value, raw_value.max_temp);
+                                raw_value.sum_temp += value as i64;
+                                raw_value.temp_reading_count += 1;
+                            }
+                            None => {
+                                let reading = TotalReading::new(name.to_owned(), value);
+                                table.insert(name.to_owned(), reading);
+                            }
+                        };
+                    };
+                    buffer.clear();
+                }
+                _ => buffer.push(*element as char),
+            };
+        }
+        self.data.merge(table);
+    }
 }
